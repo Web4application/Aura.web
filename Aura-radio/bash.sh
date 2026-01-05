3a65305079cd67a49c5a85412faa3e2cd26e1891
Seriki yakub <sandraisom00@gmail.com>
2026-01-05 17:07:32 +0100

    Create bash.sh

---
+++ Aura-radio/bash.sh
@@ -0,0 +1,13 @@
+---
+'aura-radio/
+├─ web/                 # Your static AURA client (HTML/CSS/JS)
+│  ├─ index.html
+│  ├─ aura-web-client.css
+│  └─ aura-web-client.js
+├─ ws-server/           # Node.js WebSocket server
+│  ├─ package.json
+│  └─ ws-server.js
+├─ icecast/             # Icecast configuration
+│  └─ icecast.xml
+└─ docker-compose.yml'
+---
